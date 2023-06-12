a3ceb4a70cc9288deb73da18c4ed1170fb25818c
Aryan Saini <aryansaini1005@gmail.com>
2023-06-12 16:25:53 -0400

    dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Auth from "./pages/auth/Auth";
 import SignIn from "./pages/auth/sign-in/Sign-In";
 import SignUp from "./pages/auth/sign-up/Sign-Up";
 import { ActivateAccount } from "./pages/auth/activate-account/ActivateAccount";
+import Dashboard from "./pages/backend/Dashboard";
 
 const App = () => {
   return (
     <>
     <Routes>
-      <Route path="*" element={<h1>Error 404 sirjak</h1>}></Route>
+      <Route path="*" element={<h1>Error 404</h1>}></Route>
       <Route path="/auth" element={<Auth />}>
         <Route index element={<SignIn/>} />
         <Route path="sign-up">
@@ -16,7 +17,9 @@ const App = () => {
           <Route path="activate" element={<ActivateAccount/>}/>
         </Route>
       </Route>
-      <Route path="/" element={<Link to="/auth">Auth</Link>} />
+      <Route path="/dashboard" element={<Dashboard />}>
+      </Route>
+      <Route path="/" element={<><Link to="/auth">Auth</Link> <Link to="/dashboard">dashboard</Link> </>} />
     </Routes>
     </>
   )
